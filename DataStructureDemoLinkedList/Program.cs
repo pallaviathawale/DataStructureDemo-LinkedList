8c7f37a63914dc36e3cf749703c944aa9fff3390
“pallaviathawale” <athawalepallavi1998@gmai.com>
2023-03-10 09:14:04 +0530

    UC1-CreateSimpleLinkedList

--- DataStructureDemoLinkedList/Program.cs
+++ DataStructureDemoLinkedList/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("......Data Structure Demo Problmes......");
+            LinkedList list = new LinkedList();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.Display();
         }
     }
 }
