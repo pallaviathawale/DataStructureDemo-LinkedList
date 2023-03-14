57ac10286b174c32bb0f4b00bbc862727509021e
“pallaviathawale” <athawalepallavi1998@gmai.com>
2023-03-14 18:26:03 +0530

    Add: UC4-InsertionInLinkedList

--- DataStructureDemoLinkedList/LinkedList.cs
+++ DataStructureDemoLinkedList/LinkedList.cs
@@ -11,24 +11,30 @@ namespace DataStructureDemoLinkedList
         internal Node head;
         internal void Add(int data)
         {
-            Node node = new Node(data);
-            if (this.head == null)
+            Node node = new Node(data); //object of new and push data into node
+            if (this.head == null) 
                 this.head = node;
+
             else
             {
+
                 Node temp = head;
                 while (temp.next != null)
                 {
+                    //Console.WriteLine("temp value is " +temp.next.data);
                     temp = temp.next;
+
                 }
                 temp.next = node;
             }
             Console.WriteLine("{0} inserted into the linked list", node.data);
+            // Console.WriteLine("Value of node is: " + node);
         }
 
         internal void Display()
         {
             Node temp = this.head;
+
             if (temp == null)
             {
                 Console.WriteLine("Linked list is empty");
@@ -40,5 +46,39 @@ namespace DataStructureDemoLinkedList
                 temp = temp.next;
             }
         }
+        internal Node InsertAtPerticularPosition(int position, int data) 
+       {
+            if (position < 1)
+                Console.WriteLine("Invalid Position");
+
+            if (position == 1)
+            {
+                var newNode = new Node(data); 
+                newNode.next = this.head;
+                head = newNode;
+            }
+            else
+            {
+                while (position-- !=0)
+                {
+                    if(position == 1)
+                    {
+                        Node node = new(data);
+                        node.next = this.head.next;
+                        head.next = node;
+                        break;
+
+                    }
+                    head = head.next;
+                }
+                if (position != 1)
+                    Console.WriteLine("position out of range");
+            }
+            Console.WriteLine("inserted value is " + head);
+            return head;
+
+        } 
+
+      
     }
 }
--- DataStructureDemoLinkedList/Program.cs
+++ DataStructureDemoLinkedList/Program.cs
@@ -6,9 +6,11 @@
         {
             LinkedList list = new LinkedList();
             list.Add(56);
-            list.Add(30);
+            //list.Add(30);
             list.Add(70);
-            list.Display();
-        }
+            //list.Display();
+            list.InsertAtPerticularPosition(2, 30);
+                
+         }
     }
 }
